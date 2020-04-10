40ab0bcf7bad17c706648dcb3e3503eda360af5d
herts <lhz007563@gmail.com>
2020-04-10 12:23:58 +0800

    Default page removed
    Signed-off-by: herts <lhz007563@gmail.com>

--- controllers/default.go
+++ controllers/default.go
@@ -9,12 +9,6 @@ type MainController struct {
 	beego.Controller
 }
 
-func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
-}
-
 func (c *MainController) LoginPage() {
 	c.Layout = "layout.html"
 	c.TplName = "login.html"
