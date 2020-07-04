ee1f460d8fc1063737c1fadd5c194a4ebd4d8083
herts <lhz007563@gmail.com>
2020-07-04 19:00:24 +0800

    setup.sh added
    Signed-off-by: herts <lhz007563@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
-rm ray-ui-cli
-wget https://github.com/Herts/ray-ui-cli/releases/download/v0.1/ray-ui-cli
-chmod +x ray-ui-cli
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+
+sudo apt-get install certbot python3-certbot-nginx -y
+
+sudo apt-get install wget screen git -y
+
+bash <(curl -L -s https://install.direct/go.sh)
+
+git clone https://github.com/Herts/ray-ui-cli.git
+
+cd ray-ui-cli
+cp conf/v2ray.tpl /etc/v2ray/config.json
+bash update.sh
+
+
+
+
